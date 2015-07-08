aee46841ba951692febfb1b9c320476403c3f68a
Hirokazu Tanaka <gitseitanaka@users.noreply.github.com>
2015-07-08 15:35:42 +0900

    * add test code for mocha

--- test/test_echostring.js
+++ test/test_echostring.js
@@ -1,59 +1,119 @@
 var assert = require("power-assert");
 var stringecho = require('../index.js');
 
+var testfilename = 'test/teststrings.txt';
+var fs = require('fs'),
+    readline = require('readline'),
+    rs = fs.ReadStream(testfilename),
+    rl = readline.createInterface({'input': rs, 'output': {}});
+var namearray = [];
+rl.on('line', function (line) {
+    var name = line.trim();
+    if (name.length !== 0) {
+      namearray.push(name);
+    }
+});
+rl.resume();
+
 
 describe('string-echo', function () {
-	var testfilename = 'test/teststrings.txt';
-	var fs = require('fs'),
-	    readline = require('readline'),
-	    rs = fs.ReadStream(testfilename),
-	    rl = readline.createInterface({'input': rs, 'output': {}});
-	var namearray = [];
-	rl.on('line', function (line) {
-			var name = line.trim();
-			if (name.length !== 0) {
-		    namearray.push(name);
-			}
-	});
-	rl.resume();
+  var gid = 0;
+  describe('start-stop', function () {
+    
+    it ('start->stop(none call cb)', function (done) {
+      var called = 0;
+      gid = stringecho.async(testfilename, 100,
+                function(id, name) {  // process
+                  called++;
+                },
+                function(id) {        // finish
+                  assert.equal(called, 0);
+                  done();
+                }
+              );
+      stringecho.abort(gid);
+    });
 
-	describe('start-stop', function () {
-		
-		it ('start->stop(none call cb)', function () {
-			var called = 0;
-			var gid = stringecho.async(testfilename, 100,
-								function(id, name) {	// process
-									assert.equal(namearray[called], name);
-									called++;
-								},
-								function(id) {				// finish
-									assert(called === 0);
-								}
-							);
-			stringecho.abort(gid);
-		});
-
-		it ('start->stop(called cb 1 time)', function (done) {
-			var called = 0;
-			var gid = stringecho.async(testfilename, 100,
-								function(id, name) {	// process
-									assert.equal(namearray[called], name);
-									called++;
-								},
-								function(id) {				// finish
-									assert(called === 1);
-									done();
-								}
-							);
-			setTimeout(function () {
-				stringecho.abort(gid);
-			}, 150);
-		});
-
-
-	});
-
-
+    it ('start->stop(called cb 1 time)', function (done) {
+      var interval = 100 /* ms */;
+      var times = 1;
+      var called = 0;
+      gid = stringecho.async(testfilename, interval,
+                function(id, name) {  // process
+                  assert.equal(namearray[called], name);
+                  called++;
+                },
+                function(id) {        // finish
+                  assert.equal(called, 1);
+                  done();
+                }
+              );
+      setTimeout(function () {
+        stringecho.abort(gid);
+      }, interval * times);
+    });
+    
+    it ('start->stop(called cb 20 time)', function (done) {
+      var interval = 100 /* ms */;
+      var times = 20;
+      var called = 0;
+      gid = stringecho.async(testfilename, interval,
+                function(id, name) {  // process
+                  assert.equal(namearray[called % namearray.length], name);
+                  assert.equal(gid, id);
+                  called++;
+                },
+                function(id) {        // finish
+                  //assert.equal(called, times);
+                  assert.equal(gid, id);
+                  done();
+                }
+              );
+      setTimeout(function () {
+        stringecho.abort(gid);
+      }, interval * times);
+    });
+    
+    /* used default value(4) with "UV_THREADPOOL_SIZE". */
+    it ('start->stop(called cb 20 time) x 3', function (done) {
+      var interval = 200 /* ms */;
+      var times = 20;
+      var tester = 4;
+      var count = 0;
+      var func = function() {
+          return function(aGid) {
+                  var _called = 0;
+                  var _gid = stringecho.async(testfilename, interval,
+                            function(id, name) {  // process
+                              assert.equal(namearray[_called % namearray.length], name);
+                              assert.equal(aGid, id);
+                              
+                              _called++;
+                            },
+                            function(id) {        // finish
+                              count++;
+                              assert.equal(aGid, id);
+                              assert.equal(_called, times);
+//                              console.log('***', id);
+                              if (count >= tester) {
+                                done();
+                              }
+                            }
+                          );
+                  
+                    setTimeout(function () {
+                      stringecho.abort(aGid);
+                    }, interval * times);
+                  return _gid;
+            };
+          };
+      
+      var nextgid = gid + 1;
+      for (var i = 0; i < tester; i++) {
+//        console.log('');
+        var testfunc = func();
+        assert.equal(nextgid + i ,testfunc(nextgid + i));
+      }
+    });
+  });
 });
-
-
